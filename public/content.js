576f885e35d00a2f92ccf2841870e11c3cb9f987
CJ <chasanid@gmail.com>
2025-03-12 19:40:45 -0400

    fix prettier

--- public/content.js
+++ public/content.js
@@ -17,52 +17,37 @@
         chrome.storage.sync.set({ intervalActive: true });
 
         intervalId = setInterval(() => {
-          chrome.storage.sync.get(
-            ["productiveSites", "unproductiveSites", "flowerStats"],
-            (data) => {
-              console.log("data", { data });
-              let flowerStats = data.flowerStats || {
-                loading: true,
-                growth: 0,
-                wilt: 0,
-              };
-              let growth = flowerStats.growth;
-              let wilt = flowerStats.wilt;
-              chrome.runtime.sendMessage(
-                { request: "getTabUrl" },
-                (response) => {
-                  const productiveSites =
-                    data.productiveSites?.split(",") || [];
-                  const unproductiveSites =
-                    data.unproductiveSites?.split(",") || [];
-                  if (
-                    productiveSites.some((site) =>
-                      response?.url.includes(site.trim()),
-                    )
-                  ) {
-                    if (wilt > 0) {
-                      wilt--;
-                    } else {
-                      growth++;
-                    }
-                  } else if (
-                    unproductiveSites.some((site) =>
-                      response?.url.includes(site.trim()),
-                    )
-                  ) {
-                    console.log("unproductive");
-                    if (wilt < 5) {
-                      wilt++;
-                    } else {
-                      growth--;
-                    }
-                  }
-                  flowerStats = { loading: false, growth, wilt };
-                  chrome.storage.sync.set({ flowerStats });
-                },
-              );
-            },
-          );
+          chrome.storage.sync.get(["productiveSites", "unproductiveSites", "flowerStats"], (data) => {
+            console.log("data", { data });
+            let flowerStats = data.flowerStats || {
+              loading: true,
+              growth: 0,
+              wilt: 0,
+            };
+            let growth = flowerStats.growth;
+            let wilt = flowerStats.wilt;
+            chrome.runtime.sendMessage({ request: "getTabUrl" }, (response) => {
+              const productiveSites = data.productiveSites?.split(",") || [];
+              const unproductiveSites = data.unproductiveSites?.split(",") || [];
+              if (productiveSites.some((site) => response?.url.includes(site.trim()))) {
+                console.log("productive");
+                if (wilt > 0) {
+                  wilt--;
+                } else {
+                  growth++;
+                }
+              } else if (unproductiveSites.some((site) => response?.url.includes(site.trim()))) {
+                console.log("unproductive");
+                if (wilt < 5) {
+                  wilt++;
+                } else {
+                  growth--;
+                }
+              }
+              flowerStats = { loading: false, growth, wilt };
+              chrome.storage.sync.set({ flowerStats });
+            });
+          });
         }, 2500);
       });
     } else if (message.command === "stop") {
